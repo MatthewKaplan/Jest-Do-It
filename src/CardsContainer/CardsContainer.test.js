61fb35a58dc46ebde72e350ab1ca0f5db00d6b8e
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-13 12:33:35 -0600

    Further file setup

---
+++ src/CardsContainer/CardsContainer.test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { CardsContainer } from './CardsContainer';
+import { shallow } from 'enzyme';
---
+++ src/WelcomePage/WelcomePage.test.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { WelcomePage } from './WelcomePage';
+import { shallow } from 'enzyme';
