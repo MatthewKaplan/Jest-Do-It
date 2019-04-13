bc3db92633801b464f18c7f9b1835f55b004c8eb
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-13 14:43:25 -0600

    Store playerName and toggle activePlayer

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import WelcomePage from '../WelcomePage/WelcomePage.js';
 import '../Styles/Main.scss';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      activePlayer: false,
+      playerName: "",
       questions: [],
       currentQuestion: []
     }
+  }
+
+  startQuiz = (e) => {
+    e.preventDefault();
+    this.setState({
+      activePlayer: true
+    }, console.log(this.state.activePlayer))
+  }
+
+  setPlayer = (e) => {
+    this.setState({
+      playerName: e.target.value
+    }, console.log(this.state.playerName))
   }
 
 
@@ -16,8 +32,14 @@ export default class App extends Component {
 
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
+      <WelcomePage 
+        startQuiz={this.startQuiz}
+        playerName={this.state.playerName}
+        setPlayer={this.setPlayer}
+      />
         
       </div>
     );
--- src/WelcomePage/WelcomePage.js
+++ src/WelcomePage/WelcomePage.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import '../Styles/_WelcomePage.scss';
+
+const WelcomePage = (props) => {
+  return (
+    <section className="welcomePage">
+      <article className="backGroundImg">
+        <h1 className="welcomeHeader">Welcome to Jest Do It.</h1>
+        <h2 className="subHeader">An online resource for learning the testing framework Jest along with the testing utility Enzyme.</h2>
+        <form className="signInForm" onSubmit={props.startQuiz}>
+          <input
+            type='text'
+            placeholder='Enter your name'
+            value={props.playerName}
+            onChange={props.setPlayer}
+            className='nameInput'
+            data-test='userNameInput' 
+          />
+        </form>
+      </article>
+    </section>
+  )
+}
+
+export default WelcomePage;
