8723b67ec0232f56c55ddf7873cdaafc7f81c942
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-13 12:09:01 -0600

    Initial file setup

---
+++ src/Cards/Cards.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../Styles/_Cards.scss';
+
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Cards;
---
+++ src/Controls/Controls.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../Styles/_Controls.scss';
+
+class Controls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Controls;
